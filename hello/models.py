06574579bddb9bfee3f7290f20269113d7b4a981
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-09-16 14:45:50 +1200

    Updating event model

--- hello/models.py
+++ hello/models.py
@@ -18,12 +18,22 @@ class User(models.Model):
 
 
 class Post(models.Model):
-    post_ID = models.AutoField()
     post_title = models.TextField("Post Title")
     location = models.TextField("Location")
     description = models.TextField("Description")
     help_status = models.TextField("Help status")
 
     def __str__(self):
-        return self.post_ID+self.post_title
+        return self.post_title
 
+class Event(models.Model):
+    event_title = models.TextField("Event Title")
+    location = models.TextField("Location")
+    description = models.TextField("Description")
+    start_date = models.DateTimeField("Start Date")
+    end_date = models.DateTimeField("End Date")
+    start_time = models.DateTimeField("Start Time")
+    end_time = models.DateTimeField("End Time")
+    attachment = models.TextField("Attach File")
+    def __str__(self):
+        return self.event_title
