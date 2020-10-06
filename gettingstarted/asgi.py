9de1d69b76fda30aef6f3160c125b79a2181d979
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-06 16:17:18 +1300

    Trying to get Heroku to work

--- gettingstarted/asgi.py
+++ gettingstarted/asgi.py
@@ -12,6 +12,6 @@ import os
 import uvicorn
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visualroll.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bubbles-Project\gettingstarted.settings')
 
 application = get_asgi_application()
