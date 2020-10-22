3f96405cd06a5ff9eb7614583e536c619f878d5e
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-22 14:28:23 +1300

    updating database

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -130,3 +130,5 @@ MEDIA_ROOT = '/i/'
 LOGIN_REDIRECT_URL = '/home/'
 django_heroku.settings(locals())
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
